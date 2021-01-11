d9e3fe52eb0ce7995a401f53976c9dc2e7a795af
Emmabrdt <emma.bourdit@gmail.com>
2021-01-11 17:17:51 +0100

    aligne

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -10,23 +10,28 @@
 ;                                                                              #
 ; **************************************************************************** #
 
-			global	ft_strcmp
+global	ft_strcmp
 
-ft_strcmp:  mov		rax, 0
-			mov		rcx, -1
+ft_strcmp:
+	mov		rax, 0
+	mov		rcx, -1
 
-incr:		inc		rcx
+incr:
+	inc		rcx
 
-comp:		cmp		BYTE[rdi + rcx], 0
-			je		result
-			cmp		BYTE[rsi + rcx], 0
-			je		result
-			mov		dh, BYTE[rdi + rcx]
-			cmp		dh, BYTE[rsi + rcx]
-			je		incr
+comp:
+	cmp		BYTE[rdi + rcx], 0
+	je		result
+	cmp		BYTE[rsi + rcx], 0
+	je		result
+	mov		dh, BYTE[rdi + rcx]
+	cmp		dh, BYTE[rsi + rcx]
+	je		incr
 
-result:		movzx	rax, BYTE[rdi + rcx]
-			movzx	rdx, BYTE[rsi + rcx]
-			sub		rax, rdx
+result:
+	movzx	rax, BYTE[rdi + rcx]
+	movzx	rdx, BYTE[rsi + rcx]
+	sub		rax, rdx
 
-end:		ret
+end:
+	ret
