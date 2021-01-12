0e5a79780fec4dceb7372801cf3a42cd91d8f6ba
Emmabrdt <emma.bourdit@gmail.com>
2021-01-12 11:00:59 +0100

    commentaires

--- ft_read.s
+++ ft_read.s
@@ -14,16 +14,16 @@ global	ft_read
 extern	__errno_location
 
 ft_read:
-	mov		rax, 0
+	mov		rax, 0			; syscall a read
 	syscall
-	cmp		rax, 0
-	jl		erreur
-	ret
+	cmp		rax, 0			; compare si le retour du syscall est egal a 0
+	jl		error			; si rax est inferieur a 0 on va a error
+	ret					; sinon on return rax
 
-erreur:
-	neg		rax
-	mov		rdi, rax
-	call		__errno_location
-	mov		[rax], 	rdi
-	mov		rax, -1
-	ret
+error:
+	neg		rax			; car le syscall renvoie dans rax errno mais en negatif
+	mov		rdi, rax		; rdi sert de tampon car apres rax prendera le retour de errno location
+	call		__errno_location	; errno location renvoie un pointeur sur errno dans rax
+	mov		[rax], 	rdi		; d'ou ici on met rdi dans errno
+	mov		rax, -1			; on met -1 dans rax pour renvoyer la bonne valeur d'un appel a read
+	ret					; return rax
--- ft_strdup.s
+++ ft_strdup.s
@@ -18,21 +18,21 @@ extern __errno_location
 
 ft_strdup:
 	call	ft_strlen		; appelle ft_strlen(rdi) ou rdi est encore *s
-	push	rdi				; rdi contient *s dont on aura besoin après pour ft_strcpy. On push dans la pile pour pas perdre *s
-	inc		rax				; rax contient la taille renvoyée par ft_strlen, on incremente de 1 pr le \0
-	mov		rdi, rax		; rdi sera envoyé a malloc donc doit etre egal au nombre de caractere de *s cad rax
+	push	rdi			; rdi contient *s dont on aura besoin après pour ft_strcpy. On push dans la pile pour pas perdre *s
+	inc	rax			; rax contient la taille renvoyée par ft_strlen, on incremente de 1 pr le \0
+	mov	rdi, rax		; rdi sera envoyé a malloc donc doit etre egal au nombre de caractere de *s cad rax
 	call	malloc			; on appelle malloc pour malloquer la nouvelle chaine de rax caracteres (renverra le pointeur sur la place mémoire dans rax)
-	cmp		rax, 0			; si malloc echoue
-	jz		error			; si echoue on return
-	mov		rdi, rax		; on met rax dans rdi pour que rdi ai la place memoire pr ensuite envoyer a strcpy
-	pop		rsi				; je remets *s dans rsi
+	cmp	rax, 0			; si malloc echoue
+	jz	error			; si echoue on return
+	mov	rdi, rax		; on met rax dans rdi pour que rdi ai la place memoire pr ensuite envoyer a strcpy
+	pop	rsi			; je remets *s dans rsi
 	call	ft_strcpy		; copie rsi dans rdi
-	ret
+	ret				; return rax
 
 error:
-	neg	rax
-	mov	rdi,	rax
-	call	__errno_location
-	mov	[rax], rdi
-	mov	rax, -1
-	ret
+	neg	rax			; car l'appel a malloc renvoie dans rax errno mais en negatif
+	mov	rdi,	rax		; rdi sert de tampon car apres rax prendera le retour de errno location
+	call	__errno_location	; errno location renvoie dans rax un pointeur sur errno
+	mov	[rax], rdi		; d'où ici on met la valeur de rdi dans rax
+	mov	rax, -1			; on met rax à -1 pour renvoyer la bonne valeur d'un appel à strdup
+	ret				; return rax
