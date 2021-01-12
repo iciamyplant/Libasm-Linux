6e1d402789f23a9a12881405df64095ad61e3afa
Emmabrdt <emma.bourdit@gmail.com>
2021-01-12 11:36:13 +0100

    commentaires

--- libasm.h
+++ libasm.h
@@ -9,10 +9,10 @@
 #include <stdio.h>
 
 size_t		ft_strlen(char *str);
-char        *ft_strcpy(char *dest, const char *src);
-char        *ft_strdup(const char *s);
-int         ft_strcmp(const char *s1, const char *s2);
-ssize_t     ft_write(int fd, const void *buf, size_t count);
-ssize_t     ft_read(int fd, void *buf, size_t count);
+char		*ft_strcpy(char *dest, const char *src);
+char		*ft_strdup(const char *s);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t		ft_write(int fd, const void *buf, size_t count);
+ssize_t		ft_read(int fd, void *buf, size_t count);
 
 #endif
