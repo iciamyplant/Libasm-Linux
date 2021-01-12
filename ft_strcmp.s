5f81ea814409fbfabff2d2032cd27b8231ab41c0
Emmabrdt <emma.bourdit@gmail.com>
2021-01-12 11:32:21 +0100

    commentaires

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -13,25 +13,22 @@
 global	ft_strcmp
 
 ft_strcmp:
-	mov		rax, 0
-	mov		rcx, -1
-
-incr:
-	inc		rcx
+	xor		rcx, rcx		; on met rcx à 0
+	xor		rax, rax		; on met rax à 0 
 
 comp:
-	cmp		BYTE[rdi + rcx], 0
-	je		result
-	cmp		BYTE[rsi + rcx], 0
-	je		result
-	mov		dh, BYTE[rdi + rcx]
-	cmp		dh, BYTE[rsi + rcx]
-	je		incr
+	cmp		BYTE[rdi + rcx], 0	; si rdi[rcx] == '\0' --> ZF = 1
+	jz		return			; si ZF = 1 on va à return
+	cmp		BYTE[rsi + rcx], 0	; si rsi[rcx] == '\0' --> ZF = 1
+	jz		return			; si ZF = 1 on va à return
+	mov		al, BYTE[rsi + rcx]	; on met rsi[rcx] dans al pour pouvoir faire la comparaison
+	cmp		BYTE[rdi + rcx], al	; si rdi[rcx] != rsi[rcx] --> ZF = 0
+	jnz		return			; si ZF = 0 on va à return
+	inc		rcx			; sinon les deux caracteres sont egaux on incrémente rcx
+	jmp		comp			; et on continue à comparer
 
-result:
-	movzx		rax, BYTE[rdi + rcx]
-	movzx		rdx, BYTE[rsi + rcx]
-	sub		rax, rdx
-
-end:
-	ret
+return:
+	movzx		rax, BYTE[rdi + rcx]	; copie le contenu de droite(8bits) dans rax(64bits)
+	movzx		rdx, BYTE[rsi + rcx]	; copie le contenu de droite(8bits) dans rax(64bits)
+	sub		rax, rdx		; soustrait rdx à rax et met le resultat dans rax
+	ret					; return rax
